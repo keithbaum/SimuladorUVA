81f05763e17e7cbe79c27b76d5fb9dbf8737eab9
keithbaum <keith.baum@yahoo.com.ar>
2018-06-07 02:27:33 -0300

    Added mockTransition function

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import matplotlib.style
 from simulations import salarySimulationRun, calculateSettlementToSalaryRatios, printLoanReport, killDefaulted
 from loan_calculator import LoanCalculator
+from unittest import mock
 
 matplotlib.use("Qt5Agg")
 matplotlib.style.use('classic')
@@ -15,14 +16,22 @@ initialSettlementToSalaryRatio=30
 explosionRate=60
 initialCut='macri'
 
+def mockTransition(*args):
+    if hasattr(mockTransition,'index'):
+        mockTransition.index+=1
+    else:
+        mockTransition.index=0
+    return list(['macri','hiperinflacion','kirchner','kirchner','macri'])[mockTransition.index % 5]
+
 series = pickle.load( open( "salarios.p", "rb" ) )
-salaries = salarySimulationRun(totalIterations=10000,
-                              loanLength=loanLength,
-                              remainingMonthsForActualCut=remainingMonthsForActualCut,
-                              monthsPerCut=monthsPerCut,
-                              initialCut=initialCut,
-                              historicSeries=series,
-                              possibleExtensionInMonths=12)
+with mock.patch('cuts.Cuts.cutTransition',side_effect=mockTransition):
+    salaries = salarySimulationRun(totalIterations=10000,
+                                  loanLength=loanLength,
+                                  remainingMonthsForActualCut=remainingMonthsForActualCut,
+                                  monthsPerCut=monthsPerCut,
+                                  initialCut=initialCut,
+                                  historicSeries=series,
+                                  possibleExtensionInMonths=12)
 
 loanCalculator = LoanCalculator(10000,loanLength,interestRate,payment='monthly',loanTimeIn='monthly')
 indexWhereRefinanced,settlementToSalaryRatios = calculateSettlementToSalaryRatios(salaries=salaries,
